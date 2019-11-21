4e0056e66535d1fe194890627cc850df07dcb6e0
Your Name <you@example.com>
2019-11-21 22:55:37 +0700

    asdasdasd

--- src/components/InfoRoom.js
+++ src/components/InfoRoom.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './InfoRoom.css';
 import { connect } from 'react-redux';
 import _ from 'lodash'
 import classNames from 'classnames'
 
 function InfoRoom(props) {
+  const ahihiRef = useRef();
+  const [indexDiv, setIndexDiv] = useState(-1);
+  const [divStyle, setdivStyle] = useState({});
   const ListRoomArr = _.map(_.find(props.dataRoom, { nameRoom: props.currentRoom }).data, (item) => ({
     avatar: item.avatar,
     name: item.name,
     level: item.level
   }))
   const lightTheme = props.changeTheme;
+  const showInfoDiv = (e, index) => {
+    setdivStyle({
+      position: 'absolute',
+      top: e.target.offsetTop,
+    })
+    setIndexDiv(index)
+    console.log(e.target.offsetTop)
+    console.log(e.target)
+    console.log(ahihiRef)
+  }
   return (
     <div
       className={classNames('InfoRoom', {
@@ -18,7 +31,7 @@ function InfoRoom(props) {
         InfoRoomLightTheme: lightTheme === true,
       })}
     >
-      <div className='Header-InfoRoom'>
+      <div className='Header-InfoRoom' >
         {
           props.changeVietNamLanguage ?
             <p>Info Room</p>
@@ -32,7 +45,7 @@ function InfoRoom(props) {
         })}
         id="scrollbar">
 
-        <div className='Admin'>
+        <div className='Admin' >
           <div className='Title-InfoRoom'>
             <p className='Title-InfoRoom-Admin'>
               {
@@ -53,7 +66,7 @@ function InfoRoom(props) {
           </div>
         </div>
 
-        <div className='Admin'>
+        <div className='Admin' ref={ahihiRef}>
           <div className='Title-InfoRoom'>
             <p className='Title-InfoRoom-Admin'>
               {
@@ -64,16 +77,25 @@ function InfoRoom(props) {
               }
             </p>
           </div>
-          <div className='Member'>
+          <div className='Member' >
             {_.map(ListRoomArr, (item, index) =>
-              <div className='User-InfoRoom' key={index}>
+              <div className='User-InfoRoom' key={index} onClick={(e) => showInfoDiv(e, index)} >
                 <div className='Avatar-InfoRoom'>
                   <img src={item.avatar} alt='Img-User' />
                 </div>
                 <div className='Name-User-InfoRoom'>
                   <p>{item.name}</p>
                 </div>
-                <div className='AhihiTest'></div>
+                <div
+                  className={classNames('AhihiTest', {
+                    addZindexCss: index === indexDiv,
+                  })}
+                  style={divStyle}>
+                  <img src={item.avatar} alt='Img-User' />
+                  <div className='name-show-info'>
+                    <p>{item.name}</p>
+                  </div>
+                </div>
               </div>)
             }
           </div>
