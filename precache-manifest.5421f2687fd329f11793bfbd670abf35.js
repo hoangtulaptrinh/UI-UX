d1625e81da895c2da38c1cea0b37d2d0a7e2650a
Do Tung Duong <duong98kk@gmail.com>
2019-11-22 08:43:53 +0700

    Updates

---
+++ precache-manifest.5421f2687fd329f11793bfbd670abf35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064ee31ed7416f6daff8e74aed46b1cc",
+    "url": "/UI-UX/index.html"
+  },
+  {
+    "revision": "60383f904329f0e99582",
+    "url": "/UI-UX/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "8344895aaa2c50b7e9d0",
+    "url": "/UI-UX/static/css/main.f62dcc4e.chunk.css"
+  },
+  {
+    "revision": "60383f904329f0e99582",
+    "url": "/UI-UX/static/js/2.91782336.chunk.js"
+  },
+  {
+    "revision": "8344895aaa2c50b7e9d0",
+    "url": "/UI-UX/static/js/main.10989fe8.chunk.js"
+  },
+  {
+    "revision": "c7afcb926c5ba8fcba03",
+    "url": "/UI-UX/static/js/runtime-main.de7b71ad.js"
+  }
+]);
